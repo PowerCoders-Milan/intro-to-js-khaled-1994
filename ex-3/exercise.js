0616a51a68396c062757e970a7a6833366b6f025
khaled sidawi <khaledassa123@gmail.com>
2021-02-25 14:46:38 +0100

    done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,10 @@
-// Example
-window.alert("Hello World!");
+var x= 2
+var y = 3
+if (x < y){
+console.log("x smallar than y")
+} else if (x > y){ 
+    console.log("x greater than y")
+}
+else {
+    console.log("the numbers are same")
+}
