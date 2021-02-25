a301967a881a122f563641bf97adc2bb47f49340
khaled sidawi <khaledassa123@gmail.com>
2021-02-25 13:13:00 +0100

    done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,16 @@
 // Example
-window.alert("Hello World!");
+
+// the programm 
+var mybirth = 1994
+var afterwhile = 2030
+let currentDate = new Date();
+let cDay =  currentDate.getDate();
+let cMonth = currentDate.getMonth() + 1;
+let cYear =  currentDate.getFullYear();
+console.log(  cDay + "/" + cMonth + "/" + cYear );
+console.log( "my birthday : " + mybirth + "   in " +  afterwhile + " i will be : " + ( afterwhile - mybirth));
+
+
+
+
+
