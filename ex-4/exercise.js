6ec3490d827510ccc207ae8b23099d71ea1dc095
khaled sidawi <khaledassa123@gmail.com>
2021-02-25 15:43:48 +0100

    done

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,12 @@
-// Example
-window.alert("Hello World!");
+var name2 = prompt("wellcome dear")
+window.alert("hello " + name2)
+var messag1 = prompt("how your day was")
+
+if (messag1 === 'good'){
+ alert("i'm so happy for you");
+}else if ( messag1 ==='bad'){
+ alert("tomorow will be good");
+} 
+else{
+    alert ("thankyou");
+}
