559ab1d7875c1dc061df14484213df7c41a0d06e
khaled sidawi <khaledassa123@gmail.com>
2021-02-25 16:59:39 +0100

    done

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -1,2 +1,14 @@
 // Example
-window.alert("Hello World!");
+var state1 = prompt('Please enter your word')
+alert(state1 + ' Have  ' + state1.length + '  charcters');
+
+// compare between another statement
+var state2 = prompt('please your word')
+var state3 = prompt('please your second word');
+if(state2.length > state3.length){
+    alert('the charctors of '+ state2.length + ' greater than ' + state3.length + ' charctor')
+
+}
+else if (state3.length > state2.length){
+    alert('the charctors of '+ state3.length + ' greater than ' + state2.length + ' charctor')
+}
