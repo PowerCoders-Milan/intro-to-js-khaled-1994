8d7a90cfd05b2cbe32a5b45c9636d7d971b428de
khaled sidawi <khaledassa123@gmail.com>
2021-02-25 12:06:27 +0100

    done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,8 @@
 // Example
-window.alert("Hello World!");
+window.alert("Hello World")
+// the program
+var  numChild = 3
+var  patner = " muha "
+var  geoLocation = " Roma "
+var  jobTi = " developer "
+console.log( "MY JOB IS : " + jobTi + " MY geographic location : "  + geoLocation + " MY partner's name : " + patner + "MY  NYMBER CHINDREN : " + numChild)
